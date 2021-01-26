fa4f748924a1514986e103eb966a3f58590f9bf7
Faith <oyebodefaith3@gmail.com>
2021-01-26 08:40:43 +0100

    created user profile update functionality (route and controller)

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,10 @@
 import express from 'express';
-import { authUser, registerUser, getUserProfile } from '../controllers/userController.js';
+import { 
+    authUser, 
+    registerUser, 
+    getUserProfile,
+    updateUserProfile 
+} from '../controllers/userController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
 
@@ -7,7 +12,7 @@ const router = express.Router();
 
 router.route('/').post(registerUser); 
 router.post('/login', authUser); 
-router.route('/profile').get(protect, getUserProfile); 
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile); 
 
 
 export default router;
