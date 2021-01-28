730ffc170860f655c87338848fd0fd3e616d0b6f
Faith <oyebodefaith3@gmail.com>
2021-01-28 19:01:35 +0100

    created order route and controller

---
+++ routes/orderRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import { 
+    addOrderItems 
+} from '../controllers/orderController.js';
+import { protect } from '../middlewares/authMiddleware.js';
+
+
+const router = express.Router();
+
+router.route('/').post(protect, addOrderItems); 
+ 
+
+
+export default router;
