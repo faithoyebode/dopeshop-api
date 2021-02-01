7dbd196dc0f9859e5bae0f511c26792f4487783f
Faith <oyebodefaith3@gmail.com>
2021-02-01 19:22:56 +0100

    created pay order route and controller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -43,7 +43,6 @@ const addOrderItems = asyncHandler(async (req, res) => {
 //@access Private
 const getOrderById = asyncHandler(async (req, res) => {
     const order = await Order.findById(req.params.id).populate('user', 'name email');
-    console.log(order);
     if(order){
         res.json(order);
     }else{
@@ -53,4 +52,33 @@ const getOrderById = asyncHandler(async (req, res) => {
 });
 
 
-export { addOrderItems, getOrderById };
+//@desc Update order to paid
+//@route GET /api/orders/:id/pay
+//@access Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
+    if(order){
+        order.isPaid = true;
+        order.paidAt = Date.now();
+        order.paymentResult = {
+            id: req.body.id,
+            status: req.body.status,
+            update_time: req.body.update_time,
+            email_address: req.body.payer.email_address
+        }
+
+        const updatedOrder = await order.save();
+
+        res.json(updatedOrder);
+    }else{
+        res.status(404);
+        throw new Error('Order not found');
+    }
+});
+
+
+export { 
+    addOrderItems, 
+    getOrderById,
+    updateOrderToPaid 
+};
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import { 
     addOrderItems,
-    getOrderById 
+    getOrderById, 
+    updateOrderToPaid
 } from '../controllers/orderController.js';
 import { protect } from '../middlewares/authMiddleware.js';
 
@@ -10,6 +11,8 @@ const router = express.Router();
 
 router.route('/').post(protect, addOrderItems); 
 router.route('/:id').get(protect, getOrderById); 
+router.route('/:id/pay').get(protect, updateOrderToPaid); 
+
 
  
 
