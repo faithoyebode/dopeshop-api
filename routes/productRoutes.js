3266f6a9930027f85d881127ef01f30e3763be9e
Faith <oyebodefaith3@gmail.com>
2021-02-10 13:07:21 +0100

    added product-create and product-update routes for admin

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,23 @@
 import express from 'express';
-import { getProducts, getProductById, deleteProduct } from '../controllers/productController.js';
+import { 
+    getProducts, 
+    getProductById, 
+    deleteProduct,
+    createProduct,
+    updateProduct
+} from '../controllers/productController.js';
 import { protect, admin } from '../middlewares/authMiddleware.js';
  
 
 const router = express.Router();
 
-router.route('/').get(getProducts); 
+router.route('/')
+.get(getProducts)
+.post(protect, admin, createProduct); 
+ 
 router.route('/:id')
     .get(getProductById)
-    .delete(protect, admin, deleteProduct); 
+    .delete(protect, admin, deleteProduct)
+    .put(protect, admin, updateProduct); 
 
 export default router;
