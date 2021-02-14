0947c683789856b40346d7e8af9388da0c55e4ef
Faith <oyebodefaith3@gmail.com>
2021-02-14 18:22:01 +0100

    added pagination capability to getProducts route

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,6 +6,8 @@ import asyncHandler from 'express-async-handler';
 //@route GET /api/products
 //@access Public
 const getProducts = asyncHandler(async (req, res) => {
+    const pageSize = 10;
+    const page = Number(req.query.pageNumber) || 1;
     const keyword = req.query.keyword ? 
         {
             name: {
@@ -14,9 +16,10 @@ const getProducts = asyncHandler(async (req, res) => {
             }
         } : {}
     
-    const products = await Product.find({...keyword});
+        const count = await Product.countDocuments({ ...keyword });
+    const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize * (page - 1));
 
-    res.json(products);
+    res.json({products, page, pages: Math.ceil(count / pageSize) });
 });
 
 
